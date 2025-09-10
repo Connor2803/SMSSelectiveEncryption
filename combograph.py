b8eadcac0802ac82493c80bd0fb8b4a8fe427705
Connor Grayden <87635341+Connor2803@users.noreply.github.com>
2025-09-10 15:36:34 +0800

    Rerun parts for more accurate results

--- combograph.py
+++ combograph.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 
-IN_CSV = "results8.csv"
+IN_CSV = "resultsAVG8.csv"
 OUT_DIR = "plots"
 
 def parse_time_to_seconds(s: str) -> float:
